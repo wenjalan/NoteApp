162100eb7b06d6498ca43c3a609c50d5c7389795
Alan Wen <alanjameswen@outlook.com>
2022-01-09 18:42:39 -0800

    Markdown Demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 
+// Main App Component
 function App() {
+  return (<MarkdownTest />);
+}
+
+// Markdown Side-by-Side Demo
+function MarkdownTest() {
+  // text input state
+  const [input, setInput] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <InputBox setInput={setInput} />
+      <DisplayBox text={input} />
     </div>
   );
+}
+
+// Markdown Raw Input Box
+function InputBox(props) {
+  let onChange = (e) => {
+    // set input to value of input box
+    props.setInput(e.target.value);
+  }
+  return (
+    <div className="input-box-container">
+      <h1>Raw Input</h1>
+      <textarea className="raw-input-textarea" onChange={onChange}/> 
+    </div>
+  )
+}
+
+// Markdown Display Box
+function DisplayBox(props) {
+  const text = props.text;
+  return (
+    <div className="display-box-container">
+      <h1>Display</h1>
+      <ReactMarkdown className="display-markdown">
+        {text}
+      </ReactMarkdown>
+    </div>
+  )
 }
 
 export default App;
---
+++ test.md
@@ -0,0 +1,15 @@
+hello
+
+*this is italic*
+
+**this is bold**
+
+- this is
+- a list
+- of a few items
+
+# heading1
+## heading2
+### heading3
+
+[somelink](someurl)
