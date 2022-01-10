fa5ff1aea6e9f4a2d6ff228f945f0a3bb14098ca
Alan Wen <alanjameswen@outlook.com>
2022-01-10 15:11:36 -0800

    selection position tracking

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
 
 // Main App Component
@@ -11,26 +11,48 @@ function App() {
 
 // Markdown Side-by-Side Demo
 function MarkdownTest() {
-  // text input state
+  // Input Box State
   const [input, setInput] = useState('');
+  const [inputPosition, setInputPosition] = useState({
+    start: undefined,
+    end: undefined
+  });
+
+  // component
   return (
     <div className="app-container">
-      <InputBox setInput={setInput} />
-      <DisplayBox text={input} />
+      <Caret/>
+      <InputBox setInputPosition={setInputPosition} setInput={setInput}/>
+      <DisplayBox text={input}/>
     </div>
   );
 }
 
 // Markdown Raw Input Box
 function InputBox(props) {
+  // on text change
   let onChange = (e) => {
     // set input to value of input box
     props.setInput(e.target.value);
+
+    // set caret position
+    props.setInputPosition({
+      start: e.target.selectionStart,
+      end: e.target.selectionEnd
+    });
+  }
+  // on selection change
+  let onSelectionChange = (e) => {
+    // set caret position
+    props.setInputPosition({
+      start: e.target.selectionStart,
+      end: e.target.selectionEnd
+    });
   }
   return (
     <div className="input-box-container">
       <h1>Raw Input</h1>
-      <textarea className="raw-input-textarea" onChange={onChange}/> 
+      <textarea className="raw-input-textarea" onChange={onChange} onSelect={onSelectionChange}/> 
     </div>
   )
 }
@@ -46,6 +68,14 @@ function DisplayBox(props) {
       </ReactMarkdown>
     </div>
   )
+}
+
+// Caret
+// TODO: Make the caret render at the correct position on the DisplayBox
+function Caret() {
+  return (
+    <span className='caret'/>
+  );
 }
 
 export default App;
